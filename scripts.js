203bf4d0bc136c77348721f1df283a64d5bcd037
truongvo25 <truong.hu21p0457@gmail.com>
2025-02-12 22:55:44 +0700

    scripts.js

--- scripts.js
+++ scripts.js
@@ -21,20 +21,3 @@ $('a').click(function(event){
     }, 700);
     event.preventDefault();
 }); 
-
-// Lấy số lượt truy cập từ localStorage
-let visitCount = localStorage.getItem('visitCount');
-
-// Nếu chưa có số lượt truy cập, khởi tạo nó
-if (visitCount === null) {
-    visitCount = 0;
-}
-
-// Tăng số lượt truy cập lên 1
-visitCount++;
-
-// Lưu số lượt truy cập mới vào localStorage
-localStorage.setItem('visitCount', visitCount);
-
-// Hiển thị số lượt truy cập trên trang web
-document.getElementById('visit-count').textContent = visitCount;
